b7f8d8576ba47de5b33f791971eb98e6cfa11dcb
James Rae <James.Rae@ec.gc.ca>
2025-01-14 10:25:05 -0500

    basic map

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 // without this import, this file doesn't end up in the build
 import './css/style.css';
 
-import { GoodTimes } from '@/internal';
+import EsriMap from '@arcgis/core/Map';
+import EsriMapView from '@arcgis/core/views/MapView';
 
 // everything after is what executes at startup
 
-const givver = new GoodTimes();
-givver.doWork();
+const map = new EsriMap({
+    basemap: 'terrain'
+});
+
+const view = new EsriMapView({ map, container: 'map', center: [-87, 47.5], zoom: 10 });
