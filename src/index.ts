e2beb97874cf50e8ea31527db44c9189fd55a9c4
James Rae <James.Rae@ec.gc.ca>
2025-01-14 14:28:52 -0500

    basic layer

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import './css/style.css';
 
 import EsriMap from '@arcgis/core/Map';
 import EsriMapView from '@arcgis/core/views/MapView';
+import EsriGraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
+import EsriGraphic from '@arcgis/core/Graphic';
+import EsriPoint from '@arcgis/core/geometry/Point';
+import EsriSimpleMarkerSymbol from '@arcgis/core/symbols/SimpleMarkerSymbol';
 
 // everything after is what executes at startup
 
@@ -11,3 +15,23 @@ const map = new EsriMap({
 });
 
 const view = new EsriMapView({ map, container: 'map', center: [-87, 47.5], zoom: 10 });
+
+const spikeLayer = new EsriGraphicsLayer({ id: 'spike' });
+map.add(spikeLayer);
+
+const point = new EsriPoint({
+    longitude: -87,
+    latitude: 47.5
+});
+
+const markerSymbol = new EsriSimpleMarkerSymbol({
+    color: [226, 119, 40]
+});
+
+// Create a graphic and add the geometry and symbol to it
+const spike1 = new EsriGraphic({
+    geometry: point,
+    symbol: markerSymbol
+});
+
+spikeLayer.add(spike1);
